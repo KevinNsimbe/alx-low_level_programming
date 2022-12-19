b33cd85ca77dcc35c6376d1aa827b20b6f8b57ca
KevinNsimbe <kevinnsimbe5@gmail.com>
2022-12-19 11:17:55 -0800

    getting fun, counting character

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - return the length of a string.
+ * @s: char to check
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int x = 0; /* valuable to start counter */
+
+	for (; *s++;)
+		x++;
+	return (a);
+}
