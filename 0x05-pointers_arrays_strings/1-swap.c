fee80c04aa4002c81c919b6bb72f7345819c684e
KevinNsimbe <kevinnsimbe5@gmail.com>
2022-12-19 11:00:45 -0800

    testing

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,10 +3,14 @@
  * swap_int - swap two pointers
  * @a : first in
  * @b: second int
+ * Return 0
  */
 
 void swap_int(int *a, int *b)
 {
-	*a = b;
-	*b = a;
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
 }
