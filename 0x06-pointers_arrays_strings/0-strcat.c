4979d91564ec31d780ead59d6811780c922288fe
KevinNsimbe <kevinnsimbe5@gmail.com>
2022-12-21 06:51:22 -0800

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _strcat -  a function that concatenates two strings.
+ * @src: to be appended
+ * @dest: to be appended
+ * Return: a pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	*_strcat("", '\0');
+	return (0);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MIAN_H
+
+char *_strcat(char *dest, char *src);
+
+
+#endif
